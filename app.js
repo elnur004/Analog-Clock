baabc5f84643378c54ce8405bcab908e477b0e63
elnur004 <lnur004@gmail.com>
2020-07-24 01:12:47 +0400

    added objects and methods to get the local time on console

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ let hourPosition = 44;
 let minutePosition = 146;
 let secondPosition = 304;
 
+let date = new Date();
+console.log(date);          // To get the full local date on console (not a very useful)
+
+let hour = date.getHours();
+let minute = date.getMinutes();
+let second = date.getSeconds();
+console.log(`Hour: ${hour} Minute: ${minute} Second: ${second}`);   // To get the local date on console instead
+
 HOUR.style.transform = `rotate(${hourPosition}deg)`;
 MINUTE.style.transform = `rotate(${minutePosition}deg)`;
 SECOND.style.transform = `rotate(${secondPosition}deg)`;
