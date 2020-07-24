2ac8e7650774d5b4349c2c2665284b10447981c5
elnur004 <lnur004@gmail.com>
2020-07-25 02:58:19 +0400

    changed the logic

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const HOUR = document.querySelector('#hour');
 const MINUTE = document.querySelector('#minute');
 const SECOND = document.querySelector('#second');
 
-let date = new Date();
+/**********
+ * Didn't add these variables inside the function. Because when arms reached the top, they reset to zero(spun back) everytime.
+ **********/
+
+    let date = new Date();
     console.log(date);          // To get the full local date on console (not a very useful)
     
     let hour = date.getHours();
@@ -15,7 +19,15 @@ let date = new Date();
     let secondPosition = second * 360 / 60;
 
 function runTheClock() {
+    /******
+    Inside this function we need to figure out exactly how many degrees each of these arms should move.
+    Because now, we no longer relying on the 'dates' to tell us that position.
+    And need to do this with pure math.
+    *******/
     
+    hourPosition =  hourPosition + (3 / 360);     // 360 / 12 = 30 (which is 30 devided 3600 seconds, so 30/3600 and take away the zeros)
+    minutePosition = minutePosition + (6 / 60);   // (1/60) * 60 (Minute arm moves distance of one second over 60 seconds)
+    secondPosition = secondPosition + 6;          // 360 / 60 = 6 (clock face has 360 degrees and divide by 60 seconds = 6 degrees)
     
     HOUR.style.transform = `rotate(${hourPosition}deg)`;
     MINUTE.style.transform = `rotate(${minutePosition}deg)`;
