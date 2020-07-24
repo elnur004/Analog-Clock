3f6e06cf5da02e33d63ad6e20a25dbfe3fc36516
elnur004 <lnur004@gmail.com>
2020-07-25 01:27:06 +0400

    changed the logic

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const HOUR = document.querySelector('#hour');
 const MINUTE = document.querySelector('#minute');
 const SECOND = document.querySelector('#second');
 
-function runTheClock() {
-    let date = new Date();
+let date = new Date();
     console.log(date);          // To get the full local date on console (not a very useful)
     
     let hour = date.getHours();
@@ -14,6 +13,8 @@ function runTheClock() {
     let hourPosition = (hour * 360 / 12) + (minute * (360 / 60) / 12);  /* ... + (minute * (360 / 60) / 12) means the hour arm moves along with the minute arm (avoid to the weird popping up action hour arm end of the full minute) */
     let minutePosition = (minute * 360 / 60) + (second * (360 / 60) / 60);  // ... + (second * (360 / 60) / 60) same action for the minute arm as the hour one
     let secondPosition = second * 360 / 60;
+
+function runTheClock() {
     
     
     HOUR.style.transform = `rotate(${hourPosition}deg)`;
